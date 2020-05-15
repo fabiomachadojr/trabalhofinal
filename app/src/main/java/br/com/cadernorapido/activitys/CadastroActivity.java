5bd214df02e883ca41c8b08a4be1e30a211632e0
matheuslessing <39744119+matheuslessing@users.noreply.github.com>
2020-05-15 20:36:49 -0300

    Inicio
    Inicio projeto

---
+++ app/src/main/java/br/com/cadernorapido/activitys/CadastroActivity.java
@@ -0,0 +1,27 @@
+package br.com.cadernorapido.activitys;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import br.com.cadernorapido.R;
+import br.com.cadernorapido.databinding.ActivityCadastroBinding;
+import br.com.cadernorapido.viewmodels.CadastroActivityViewModel;
+
+
+public class CadastroActivity extends AppCompatActivity {
+
+    private ActivityCadastroBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_cadastro);
+
+        CadastroActivityViewModel viewModel = new CadastroActivityViewModel(this);
+        binding.setViewModel(viewModel);
+
+    }
+}
---
+++ app/src/main/java/br/com/cadernorapido/models/Usuario.java
@@ -0,0 +1,46 @@
+package br.com.cadernorapido.models;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Generated;
+
+@Entity
+public class Usuario {
+    Long id;
+    String nome;
+    String senha;
+
+    public Usuario() {
+    }
+
+    @Generated(hash = 1527160302)
+    public Usuario(Long id, String nome, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.senha = senha;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
